8cd1cc141c61b66e3ff0d8051c7a3ed74b90c885
stassyvichev <stassyvichev@gmail.com>
2018-11-10 18:59:13 +0000

    file for cron job
    file for doing classification as part of a cron job

---
+++ Code/REST/classify-pending-cron.py
@@ -0,0 +1,36 @@
+import csv,requests
+import json as jsn
+
+def main():
+    url = "https://www.buddy311.org:31102"
+
+    # get list of pending complaints
+    r = requests.get(url + "/v1/pending")
+    pendingComplaintsList = jsn.loads(r.json()).get("pending_complaints", [])
+    if not len(pendingComplaintsList):
+        print("No pending complaints found")
+        return
+    print("Received list of pending complaints:"+ str(pendingComplaintsList))
+
+    # use the /v1/classify endpoint to classify it
+    r = requests.post(url +"/v1/classify", data = {"descriptions":pendingComplaintsList})
+    responseDict = jsn.loads(r.json())
+    classificationsList = responseDict.get("service_code",[])
+    if not len(classificationsList):
+        print("Got no classifications back")
+        return
+    print("Received list of classifications:"+ str(classificationsList))
+
+    # merge the two lists together
+    results = zip(pendingComplaintsList,classificationsList)
+    print("Merged the results:" + str(results))
+
+    # save results in complete.csv, appending
+    requests.post(url + "/v1/complete", data = {"complete":results})
+    print("Saved results in complete file.")
+
+    # clear pending.csv
+    requests.delete(url + "/v1/pending")
+    print("Cleared pending file")
+
+main()
