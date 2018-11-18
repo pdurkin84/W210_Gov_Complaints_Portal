b2b1ccff5f7356287c1ad821dd82764cee9d0371
root <root@portal.w210.mids>
2018-11-17 22:35:11 -0600

    Added loggin + service files

--- Code/Buddy311/KafkaConsumer/buddy311_consumer.py
+++ Code/Buddy311/KafkaConsumer/buddy311_consumer.py
@@ -2,6 +2,13 @@ import json
 from kafka import KafkaConsumer
 import mysql.connector as mariadb
 import requests
+import logging
+
+logging.basicConfig(filename='/var/log/buddy311consumer.log',
+	filemode='a',
+	format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+	datefmt='%H:%M:%S',
+	level=logging.INFO)
 
 # Create the database connection
 mariadb_connection = mariadb.connect(host='database', user='buddy311dba', password='AlexChrisPaulStan', database='buddy311')
@@ -12,13 +19,13 @@ consumer = KafkaConsumer('buddy311', bootstrap_servers=['kafkaserver1:9092'], va
 for message in consumer:
     # message value and key are raw bytes -- decode if necessary!
 	# e.g., for unicode: `message.value.decode('utf-8')`
-	print ("Message value: ", message.value)
+	logging.info("Message value: ", message.value)
 
 	# send request to buddy311 REST server
 	response=requests.post("http://buddy311Class:31102/buddy311/v0.1/", json=message.value)
-	print("Response is: ", response.json())
+	logging.info("Response is: %s"% response.json())
 	# update the database
-	print("Response service_code is: ", response.json()['service_code'])
+	logging.info("Response service_code is: %s" % response.json()['service_code'])
 	cursor.execute("update requests set service_code=\"%s\" where service_request_id = %d" % (response.json()['service_code'], message.value['service_request_id']))
 	mariadb_connection.commit()
 
